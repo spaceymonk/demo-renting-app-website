7588f3c15946fb3ab26674d289644a9686cc5893
ozkaberktug <b.kaan.ozkan@gmail.com>
2020-12-30 09:23:05 +0300

    update

---
+++ settings.py
@@ -0,0 +1,6 @@
+DEBUG = True
+PORT = 8080
+
+DSN = """user=postgres password=123 dbname=renting_app_db"""
+
+CATEGORIES = [str(x+1) for x in range(5)]
--- utility.py
+++ utility.py
@@ -1,7 +1,6 @@
 import random
 import string
 
-
 # Random string with the combination of lower and upper case
 def get_random_string(length, letters = string.ascii_letters):
     result_str = ''.join(random.choice(letters) for i in range(length))
